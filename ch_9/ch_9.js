49824f7478d8e54e818251e38412e849fe2663e3
Mohammad Yasin <30403468+YasinOnline@users.noreply.github.com>
2021-02-02 21:42:14 +0500

    Ch_9 Assignment is Completed.

---
+++ ch_9/ch_9.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var age = prompt("What is your age?");
+var result = `Hello ${firstName} ${lastName}, your are ${age} years old.`;
+alert(result);
