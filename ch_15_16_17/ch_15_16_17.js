ce042dd5bb9d93d73e2ecbf686faf18011c066e3
Mohammad Yasin <30403468+YasinOnline@users.noreply.github.com>
2021-02-07 18:12:36 +0500

    Assignment ch_15_16_17 is completed.

---
+++ ch_15_16_17/ch_15_16_17.js
@@ -0,0 +1,18 @@
+var courses = [];
+var course1 = prompt("course 1");
+var course2 = prompt("course 2");
+var course3 = prompt("course 3");
+var course4 = prompt("course 4");
+var course5 = prompt("course 5");
+courses.push(course1);
+courses.push(course2);
+courses.push(course3);
+courses.push(course4);
+courses.push(course5);
+alert(courses);
+course1 = prompt(courses[0], courses[0]);
+course1 = prompt(courses[1], courses[1]);
+course1 = prompt(courses[2], courses[2]);
+course1 = prompt(courses[3], courses[3]);
+course1 = prompt(courses[4], courses[4]);
+alert(courses);
