2fc8f6698fd77d2c126451d08b03732ac127b937
Mohammad Yasin <30403468+YasinOnline@users.noreply.github.com>
2021-02-28 10:38:24 +0500

    Corrected code of edit array elements in ch_15_16_17.js

--- ch_15_16_17/ch_15_16_17.js
+++ ch_15_16_17/ch_15_16_17.js
@@ -3,16 +3,20 @@ var course1 = prompt("course 1");
 var course2 = prompt("course 2");
 var course3 = prompt("course 3");
 var course4 = prompt("course 4");
-var course5 = prompt("course 5");
+var course5 = prompt("course 5"); 
+
 courses.push(course1);
 courses.push(course2);
 courses.push(course3);
 courses.push(course4);
 courses.push(course5);
+
 alert(courses);
-course1 = prompt(courses[0], courses[0]);
-course1 = prompt(courses[1], courses[1]);
-course1 = prompt(courses[2], courses[2]);
-course1 = prompt(courses[3], courses[3]);
-course1 = prompt(courses[4], courses[4]);
-alert(courses);
+
+courses[0] = prompt(courses[0], courses[0]);
+courses[1] = prompt(courses[1], courses[1]);
+courses[2] = prompt(courses[2], courses[2]);
+courses[3] = prompt(courses[3], courses[3]);
+courses[4] = prompt(courses[4], courses[4]);
+
+ alert(courses);
